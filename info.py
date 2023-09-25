0abcab920e604303984b86cb57886da1cbe04a5b
MrError007 <141121641+MrError007@users.noreply.github.com>
2023-09-25 14:40:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,7 +44,7 @@ START_MESSAGE = environ.get('START_MESSAGE', script.START_TXT)
 BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", script.BUTTON_LOCK_TEXT)
 FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', script.FORCE_SUB_TEXT)
 
-WELCOM_PIC = environ.get("WELCOM_PIC", "https://graph.org/file/a76ca4242e28aa19f82a0.jpg")
+WELCOM_PIC = environ.get("WELCOM_PIC", "https://graph.org/file/d38d7d140f8f9aeb9fd51.jpg")
 WELCOM_TEXT = environ.get("WELCOM_TEXT", script.WELCOM_TEXT)
 PMFILTER = is_enabled(environ.get('PMFILTER', "True"), True)
 G_FILTER = is_enabled(environ.get("G_FILTER", "True"), True)
